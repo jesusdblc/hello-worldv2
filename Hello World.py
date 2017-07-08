944036d4b46a8025d5e5327c2a0c54bf4b7183bb
jesusdblc <jesusdblc@gmail.com>
2017-07-08 15:05:55 -0700

    Add files via upload

---
+++ Hello World.py
@@ -0,0 +1,13 @@
+Python 3.6.2rc2 (v3.6.2rc2:8913311345, Jul  7 2017, 00:35:45) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+def hello():
+	"""Print "Hello World" and return None"""
+	print("Hello World")
+
+	
+>>> hello()
+Hello World
+>>> 
